17836d76cdae2a730c377d6e6ec183194d7ec332
HunterSun2018 <HunterSun2018@github.com>
2022-01-20 17:00:52 +0800

    added template function get_widget for getting widget

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <gtkmm.h>
 #include <iostream>
+#include <string>
+#include <string_view>
+
+using namespace std;
+using namespace Gtk;
+
+template <typename WIDGET>
+auto get_widget(Glib::RefPtr<Builder> builder, string_view widget_name)
+{
+    WIDGET *widget = nullptr;
+
+    builder->get_widget<WIDGET>(widget_name.data(), widget);
+
+    return shared_ptr<WIDGET>(widget);
+}
 
 int main(int argc, char *argv[])
 {
     auto app = Gtk::Application::create("org.gtkmm.example");
 
-    auto builder = Gtk::Builder::create_from_file("gui/demo.glade");
+    auto builder = Gtk::Builder::create_from_file("gui/test.glade");
 
-    Gtk::Window * pDialog;
-    builder->get_widget<Gtk::Window>("wnd_main", pDialog);
+    auto wnd = get_widget<Window>(builder, "wnd_main");
 
-    app->run(*pDialog, argc, argv);
-
-    delete pDialog;
+    app->run(*wnd, argc, argv);
 
     return 0;
 }
